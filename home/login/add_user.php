59ec608b48d39abfb9d7e91bd24690ec43f4cb16
naganuma514 <naganuma514@icloud.com>
2020-10-27 13:20:13 +0900

    リセット

--- home/login/add_user.php
+++ home/login/add_user.php
@@ -34,58 +34,63 @@ if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'POST') {
 ?>
 <!doctype html>
 <html lang="ja">
+
 <head>
-	<meta charset="UTF-8">
-	<meta name="description" content="最新技術と自然との調和を目指す">
-	<meta name="viewport" content="width=device-width">
-	<title>Home | NOEVIER beaty studio chou chou </title>
-	<link rel="stylesheet" media="all" href="../css/style.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
-	<script src="../js/script.js"></script>
+    <meta charset="UTF-8">
+    <meta name="description" content="最新技術と自然との調和を目指す">
+    <meta name="viewport" content="width=device-width">
+    <title>Home | NOEVIER beaty studio chou chou </title>
+    <link rel="stylesheet" media="all" href="../css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+    <script src="../js/script.js"></script>
 </head>
 
 <body id="top">
-		<div id="wrapper">
-		<div id="sidebar">
-			<div id="sidebarWrap">
-				<h1>chou chou <br>ゲスト様</h1>
-				<nav id="mainnav">
-					<p id="menuWrap"><a id="menu"><span id="menuBtn"></span></a></p>
-					<div class="panel">
-						<ul>
-							<li><a href="../index.php #top">トップ</a></li>
-							<li><a href="../index.php #sec01">メッセージ</a></li>
-							<li><a href="../index.php #sec03">スタッフ</a></li>
-							<li><a href="../index.php #sec05">アクセス</a></li>
-							<li><a href="">ログイン</a></li>
-							<li><a href="">ご予約</a></li>
-							<li><a href="">お問い合わせ</a></li>
-						</ul>
-						<ul id="sns">
-							<li><a href="#" target="_blank"><img src="../images/iconFb.png" width="20" height="20" alt="FB"></a></li>
-							<li><a href="#" target="_blank"><img src="../images/iconTw.png" width="20" height="20" alt="twitter"></a></li>
-							<li><a href="#" target="_blank"><img src="../images/iconInsta.png" width="20" height="20" alt="Instagram"></a></li>
-							<li><a href="#" target="_blank"><img src="../images/iconYouTube.png" width="20" height="20" alt="You Tube"></a></li>
-						</ul>
-					</div>
-				</nav>
-			</div>
-		</div>
-		
-	</div>
+    <div id="wrapper">
+        <div id="sidebar">
+            <div id="sidebarWrap">
+                <h1>chou chou <br>ゲスト様</h1>
+                <nav id="mainnav">
+                    <p id="menuWrap"><a id="menu"><span id="menuBtn"></span></a></p>
+                    <div class="panel">
+                        <ul>
+                            <li><a href="../index.php #top">トップ</a></li>
+                            <li><a href="../index.php #sec01">メッセージ</a></li>
+                            <li><a href="../index.php #sec03">スタッフ</a></li>
+                            <li><a href="../index.php #sec05">アクセス</a></li>
+                            <li><a href="">ログイン</a></li>
+                            <li><a href="">ご予約</a></li>
+                            <li><a href="">お問い合わせ</a></li>
+                        </ul>
+                        <ul id="sns">
+                            <li><a href="#" target="_blank"><img src="../images/iconFb.png" width="20" height="20"
+                                        alt="FB"></a></li>
+                            <li><a href="#" target="_blank"><img src="../images/iconTw.png" width="20" height="20"
+                                        alt="twitter"></a></li>
+                            <li><a href="#" target="_blank"><img src="../images/iconInsta.png" width="20" height="20"
+                                        alt="Instagram"></a></li>
+                            <li><a href="#" target="_blank"><img src="../images/iconYouTube.png" width="20" height="20"
+                                        alt="You Tube"></a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
 
-        <div class="content">
-    <?php if (isset($success) && $success) : ?>
+    </div>
+
+    <div class="content">
+        <?php if (isset($success) && $success) : ?>
         <p>登録に成功しました。</p>
         <p><a href="index.php">こちらからログインしてください。</a></p>
-    <?php else : ?>
+        <?php else : ?>
         <div class="form-frame">
             <legend>新規アカウント登録フォーム</legend>
             <h1>新規会員登録</h1>
             <?php if (count($err) !== 0) : ?>
-                <?php foreach ($err as $e) : ?>
-                    <p class="error">・<?php echo h($e); ?></p>
-                <?php endforeach; ?>
+            <?php foreach ($err as $e) : ?>
+            <p class="error">・<?php echo h($e); ?></p>
+            <?php endforeach; ?>
             <?php endif; ?>
             <form action="" method="post">
                 <p>
@@ -112,6 +117,7 @@ if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'POST') {
                 </p>
             </form>
         </div>
-    <?php endif; ?>
+        <?php endif; ?>
 </body>
+
 </html>
